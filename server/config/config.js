eedfa958e93cf4e2ab5eeaa6ee9b1ad559b67af7
JinHeeZeeniiLee <zniilee@gmail.com>
2021-10-31 23:17:50 +0900

    fix:config

--- server/config/config.js
+++ server/config/config.js
@@ -2,6 +2,14 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const config = {
+  development:{
+    username:'root',
+    password:process.env.DATABASE_PASSWORD,
+    database: 'whoever_test',
+    host: '127.0.0.1',
+    dialect:'mysql'
+  },
+
   production: {
     host: 'localhost',
     username: 'admin',
